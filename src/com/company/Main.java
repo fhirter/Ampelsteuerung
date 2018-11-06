0a7c601f04c125e167dd5dc206ba416255e5ec6e
Ginkodan <38794530+Ginkodan@users.noreply.github.com>
2018-11-06 17:35:47 +0100

    Erstellen von fxml und Controller für GUI

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,9 @@
 package com.company;
-
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 /**
  * Class Main: Mainmethode for the TEKO project "Ampelsteuerung".
  *
@@ -10,14 +14,24 @@ package com.company;
  * @date    04.11.2018
  */
 
-public class Main
-{
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("primaryStage.fxml"));
+        primaryStage.setTitle("Ampelsteuerung");
+        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.show();
+    }
+
 
     public static void main(String[] args)
     {
+        launch(args);
         Algorithmus crossroadSimulate = new Algorithmus();
 
         System.out.println("Start project.");
 
     }
+
 }
