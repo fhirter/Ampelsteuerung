1f15518bf64cc23bd17626e3e38793d0032721e3
Ginkodan <38794530+Ginkodan@users.noreply.github.com>
2018-11-06 18:56:09 +0100

    Erstellen von fxml und Controller für GUI

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("primaryStage.fxml"));
         primaryStage.setTitle("Ampelsteuerung");
-        primaryStage.setScene(new Scene(root, 1200, 1200));
+        primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
 
     }
