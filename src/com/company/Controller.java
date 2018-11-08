f3c581b0a107f1001170434ac6aabbcd4b4c18c9
grimmi22 <40334510+grimmi22@users.noreply.github.com>
2018-11-08 18:10:13 +0100

    -- Rafael 07.11.18 -- CheckBox and DrawBox

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -9,9 +9,15 @@ import java.awt.event.ActionEvent;
 import java.util.List;
 import java.util.logging.Handler;
 
-public class Controller{
+public class Controller {
 
     public MenuItem exitSoftware;
+    @FXML
+    CheckBox checkboxvelostripes;
+    @FXML
+    CheckBox checkboxbusway;
+    @FXML
+    CheckBox checkboxtramway;
 
 
     @FXML
@@ -20,35 +26,53 @@ public class Controller{
     private ChoiceBox<String> setnumberOfCrossing;
 
 
-    public void handleSubmitButtonAction() {
+    @FXML
+    private void handlesetnumberOfCrossing() {
 /*        String message = "Your Choice:\n";
 
     System.out.println(message);
     */
     }
 
+    @FXML
+    private void handleSubmitButtonAction() {
+        String message = "Your Choice:\n";
 
+        if (checkboxvelostripes.isSelected())
+            message += "Velo Stripes TRUE";
 
-    public void setSetchoiceOfAlgorithm(List<String> algorithm) {
+            if (checkboxbusway.isSelected())
+                message += "Bus Way TRUE";
 
-        setchoiceOfAlgorithm.getItems().addAll(algorithm);
-    }
-
-    public void setSetnumberOfCrossing(List<String> crossing){
-
-        setnumberOfCrossing.getItems().addAll(crossing);
-    }
-
-    public void handleButtonClick(){
-
-}
-
-
-    public void newProject(MouseEvent mouseEvent) {
-        // Erstellen eines neuen Projektes
-
-    }
+                if (checkboxtramway.isSelected())
+                    message += "Tramway TRUE";
 
 
 
-}
+                System.out.println(message);
+
+            }
+            @FXML
+            public void setSetchoiceOfAlgorithm (List < String > algorithm) {
+
+                setchoiceOfAlgorithm.getItems().addAll(algorithm);
+            }
+
+            public void setSetnumberOfCrossing (List < String > crossing) {
+
+                setnumberOfCrossing.getItems().addAll(crossing);
+            }
+
+            public void handleButtonClick () {
+
+            }
+
+
+            public void newProject (MouseEvent mouseEvent){
+                // Erstellen eines neuen Projektes
+
+            }
+
+
+        }
+
