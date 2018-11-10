32cd71af9fa2707a90984022f52116f1092c41e6
andreasvogt89 <30302212+andreasvogt89@users.noreply.github.com>
2018-11-10 17:12:42 +0100

    Klasse Driveway erstellt

---
+++ src/com/company/Driveway.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Driveway {
+
+    private boolean PedestrianStripes;
+    private boolean TrafficLightVehicle;
+    private boolean TrafficLightPedestrian;
+
+    public void SetPedestrainStripe (boolean PedestrianStripes) {
+            this.PedestrianStripes = PedestrianStripes;
+    }
+
+}
