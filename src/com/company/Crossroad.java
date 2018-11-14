381a0acd31e8f0f089ecb2362c18605bc8700cb8
andreasvogt89 <30302212+andreasvogt89@users.noreply.github.com>
2018-11-14 22:09:44 +0100

    Klasse Crossroad erstellt

--- src/com/company/Crossroad.java
+++ src/com/company/Crossroad.java
@@ -3,11 +3,18 @@ package com.company;
 public class Crossroad {
 
     public void generateDriveway () {
-        Driveway DrivewayNord  =  new Driveway (false,false,false);
+        Driveway DrivewayNord =  new Driveway (false,false,false);
         Driveway DrivewayEast = new Driveway(false, false, false);
         Driveway DrivewaySouth = new Driveway(false, false, false);
         Driveway DrivewayWest = new Driveway(false, false, false);
-    }
 
+
+
+
+
+
+
+
+    }
 
 }
