25375465d8d61b052ec1ebe153b2fa77ad5f93c2
andreasvogt89 <30302212+andreasvogt89@users.noreply.github.com>
2018-11-14 20:37:58 +0100

    Klasse Crossroad erstellt

--- src/com/company/Driveway.java
+++ src/com/company/Driveway.java
@@ -1,18 +1,47 @@
 package com.company;
 
+/**
+ * Class Driveway: Data model for the crossroad
+ *
+ *
+ *
+ * @version 1.0
+ * @autor   Class NIN
+ * @date   14.11.2018
+ */
+
 public class Driveway {
 
     private boolean PedestrianStripes;
-    private boolean TrafficLightVehicle;
-    private boolean TrafficLightPedestrian;
+    private boolean BicyclePath;
+    private boolean PublicTrafficRail;
+
+    public Driveway(boolean PedestrianStripes, boolean BicyclePath, boolean PublicTrafficRail) {
+    }
 
 
-    public void SetPedestrainStripe (boolean PedestrianStripes) {
+    public void SetPedestrainStripes (boolean PedestrianStripes) {
             this.PedestrianStripes = PedestrianStripes;
     }
 
     public boolean GetPedestrianStripes() {
         return PedestrianStripes;
+    }
+
+    public void SetBicyclePatch (boolean BicyclePath){
+        this.BicyclePath = BicyclePath;
+    }
+
+    public boolean GetBicyclePatch (){
+        return BicyclePath;
+    }
+
+    public void SetPublicTrafficRail (boolean PublicTrafficRail){
+        this.PublicTrafficRail = PublicTrafficRail;
+    }
+
+    public boolean GetPublicTrafficRail () {
+        return PublicTrafficRail;
     }
 
 }
