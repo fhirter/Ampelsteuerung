e52d5d6241923ebd60eed1a230e2436e434bab8f
SchweizerP <schweizer.patrick@sunrise.ch>
2018-12-02 21:23:40 +0100

    Uebergang der Ampelzustände angepasst.
    Crossroad wird neu aus dem Main als Objekt aufgerufen.

---
+++ src/Tests/TrafficLight/java/TrafficLightTestController.java
@@ -0,0 +1,46 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+
+public class TrafficLightTestController
+{
+    private TrafficLightTestModel model;
+    private TrafficLightState actState;
+
+    @FXML private RadioButton allOn;
+    @FXML private RadioButton flashYellow;
+    @FXML private RadioButton stateToRed;
+    @FXML private RadioButton stateToGreen;
+    @FXML private RadioButton dark;
+
+
+    public void setModel(TrafficLightTestModel model)
+    {
+        this.model = model;
+    }
+
+
+    public void runSimulation(ActionEvent actionEvent)
+    {
+        if(flashYellow.isSelected())
+        {
+            model.setYellow();
+        }
+        else if(stateToRed.isSelected())
+        {
+            model.setRed();
+        }
+        else if(stateToGreen.isSelected())
+        {
+            model.setGreen();
+        }
+        else if(allOn.isSelected())
+        {
+            model.setAllOn();
+        }
+        else if(dark.isSelected())
+        {
+            model.setDark();
+        }
+    }
+}
