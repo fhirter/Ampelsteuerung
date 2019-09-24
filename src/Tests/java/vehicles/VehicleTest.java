bd95fd7d6943a84a14ce25da31b8fa85e3ca232c
Fabian <fabian.hirter@edu.teko.ch>
2019-09-24 20:14:31 +0200

    added collision frame

---
+++ src/Tests/java/vehicles/VehicleTest.java
@@ -0,0 +1,62 @@
+package vehicles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleTest {
+
+    @org.junit.jupiter.api.Test
+    void detectCollision() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getArea() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setCrossroad() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setSpeed() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPosition() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setStart() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setDestination() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getWheelbase() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getLength() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getWidth() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void drive() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void driveStraight() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getStartPosition() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void turn() {
+    }
+}
